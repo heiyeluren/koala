353858addf93c424cb34247e189147d1c4ff05bb
黑夜路人 <heiyeluren@qq.com>
2021-05-19 11:08:17 +0800

    Update doc.go

--- src/koala/doc.go
+++ src/koala/doc.go
@@ -1,3 +1,15 @@
+/**
+ * Koala Rule Engine Core
+ *
+ * @package: main
+ * @desc: koala engine - Access http api list
+ *
+ * @author: heiyeluren 
+ * @github: https://github.com/heiyeluren
+ * @blog: https://blog.csdn.net/heiyeshuwu
+ *
+ */
+
 package main
 
 /*
